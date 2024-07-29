d2a48021b2466f14e437b8c04cd105b675032ca8
ThayaneBarros <thayane.barros.ext@synchro.com.br>
2024-07-29 20:02:51 -0300

    refactor Show tests to use @BeforeEach; create Show class

---
+++ SistemaDeIngressos/src/main/Show.java
@@ -0,0 +1,35 @@
+package main;
+
+public class Show {
+
+	public void setData(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCache(double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setDespesasInfraestrutura(double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setDataEspecial(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setArtista(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- SistemaDeIngressos/src/test/ShowTest.java
+++ SistemaDeIngressos/src/test/ShowTest.java
@@ -1,20 +1,30 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import main.Show;
 
 
 public class ShowTest {
+	private Show show;
+	
+	@BeforeEach
+    void setUp() {
+		show = new Show();
+        show.setData("29/07/2024");
+        show.setArtista("Artista");
+        show.setCache(1000.0);
+        show.setDespesasInfraestrutura(2000.0);
+        show.setDataEspecial(true);
+    }
 	
 	@Test
 	void testCriacaoShow() {
-		Show show = new Show();
-		show.setData("29/07/2024");
-		show.setData("Artista teste");
-		show.setCache(1000.0);
-		show.setDespesasInfraestrutura(2000.0);
-		show.setDataEspecial(true);
+		assertEquals("29/07/2024", show.getData());
+		
 		
 	}
 
