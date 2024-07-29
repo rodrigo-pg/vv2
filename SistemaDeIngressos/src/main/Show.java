0ba84f74680ef7263cbaee2419895a66495b20c5
ThayaneBarros <thayane.barros.ext@synchro.com.br>
2024-07-29 20:37:06 -0300

    test: ajuste ShowTest para criar show com construtor; atualizar classe Show para suportar os testes

--- SistemaDeIngressos/src/main/Show.java
+++ SistemaDeIngressos/src/main/Show.java
@@ -1,33 +1,71 @@
 package main;
 
+import java.util.List;
+
 public class Show {
 
-	public void setData(String string) {
-		// TODO Auto-generated method stub
+	private String data;
+	private String artista;
+	private double cache;
+	private double despesasInfraestrutura;
+	private List loteIngressos;
+	private boolean isDataEspecial;
+	
+	public Show(String data, String artista, double cache, double despesasInfraestrutura, List loteIngressos, boolean isDataEspecial) {
+		this.data = data;
+		this.artista = artista;
+		this.cache = cache;
+		this.despesasInfraestrutura = despesasInfraestrutura;
+		this.loteIngressos = loteIngressos;
+		this.isDataEspecial = isDataEspecial;
+	}
+
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public void setCache(double cache) {
+		this.cache = cache;
 		
 	}
 
-	public void setCache(double d) {
-		// TODO Auto-generated method stub
+	public void setDespesasInfraestrutura(double despesas) {
+		this.despesasInfraestrutura = despesas;
 		
 	}
 
-	public void setDespesasInfraestrutura(double d) {
-		// TODO Auto-generated method stub
+	public void setDataEspecial(boolean isDataEspecial) {
+		this.isDataEspecial = isDataEspecial;
 		
 	}
 
-	public void setDataEspecial(boolean b) {
-		// TODO Auto-generated method stub
-		
+	public void setArtista(String artista) {
+		this.artista = artista;
 	}
 
-	public void setArtista(String string) {
-		// TODO Auto-generated method stub
-		
+	public String getData() {
+		return this.data;
 	}
 
-	public Object getData() {
+	public String getArtista() {
+		return this.artista;
+	}
+
+	public Object getCache() {
+		return this.cache;
+	}
+
+	public Object getDespesasInfraestrutura() {
+		return this.despesasInfraestrutura;
+	}
+
+	public Boolean isDataEspecial() {
+		return this.isDataEspecial;
+	}
+
+
+	public Object getLoteIngressos() {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- SistemaDeIngressos/src/test/ShowTest.java
+++ SistemaDeIngressos/src/test/ShowTest.java
@@ -1,6 +1,10 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,19 +17,19 @@ public class ShowTest {
 	
 	@BeforeEach
     void setUp() {
-		show = new Show();
-        show.setData("29/07/2024");
-        show.setArtista("Artista");
-        show.setCache(1000.0);
-        show.setDespesasInfraestrutura(2000.0);
-        show.setDataEspecial(true);
+		show = new Show("29/07/2024", "Artista Teste", 1000.0, 2000.0, new ArrayList<>(), true);
     }
 	
 	@Test
 	void testCriacaoShow() {
 		assertEquals("29/07/2024", show.getData());
-		
-		
+		assertEquals("Artista Teste", show.getArtista());
+        assertEquals(1000.0, show.getCache());
+        assertEquals(2000.0, show.getDespesasInfraestrutura());
+        assertNull(show.getLoteIngressos());
+        assertTrue(show.isDataEspecial());	
 	}
+	
+	
 
 }
