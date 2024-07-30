989ab9f00fba6af3e36f47e3c7f86fc83e20ce32
rodrigopg <rodrigopgrodrigues@outlook.com>
2024-07-30 19:47:06 -0300

    test: Dado que a conta do pagamento possui uma data igual a fatura, quando ele for efetuado, o valor do pagamento deve ser contabilizado

--- test/contas/PagamentoTransferenciaStrategyTest.java
+++ test/contas/PagamentoTransferenciaStrategyTest.java
@@ -30,4 +30,25 @@ class PagamentoTransferenciaStrategyTest {
         assertEquals(0, pagamento.getValor().intValue());
     }
 
+    @Test
+    @DisplayName("Dado que a conta do pagamento possui uma data igual a fatura, quando ele for efetuado, o valor do pagamento deve ser contabilizado")
+    void testValorAposDataIgualDataFatura() {
+        LocalDate dataConta = LocalDate.parse("2024-05-26");
+        LocalDate dataPagamento = LocalDate.parse("2024-05-26");
+        LocalDate dataFatura = LocalDate.parse("2024-05-26");
+        Fatura fatura = Fatura.build("Rodrigo", 700.0, dataFatura);
+        PagamentoTransferenciaStrategy pagamentoBoletoStrategy = new PagamentoTransferenciaStrategy();
+        Conta conta = Conta.build(
+                1L,
+                500.0,
+                dataConta
+        );
+        double valor = 600;
+        Pagamento pagamento = Pagamento.build(valor, dataPagamento, pagamentoBoletoStrategy, 1L, 1L);
+
+        pagamentoBoletoStrategy.efetuarPagamento(pagamento, conta, fatura);
+
+        assertEquals(valor, pagamento.getValor().intValue());
+    }
+
 }
