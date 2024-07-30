79c24c7bafadb1a6235d51b2473776c57a04157f
rodrigopg <rodrigopgrodrigues@outlook.com>
2024-07-30 10:24:03 -0300

    test: Dado que uma data foi fornecido na criação de um pagamento, essa mesma data deveria constar no pagamento

--- src/contas/Pagamento.java
+++ src/contas/Pagamento.java
@@ -1,23 +1,34 @@
 package contas;
 
+import java.time.LocalDate;
+
 public class Pagamento {
     private Double valor;
+    private LocalDate data;
 
     private Pagamento(
-            Double valor
+            Double valor,
+            LocalDate data
     ) {
         this.valor = valor;
+        this.data = data;
     }
 
     public Double getValor() {
         return valor;
     }
 
+    public LocalDate getData() {
+        return data;
+    }
+
     public static Pagamento build(
-            Double valor
+            Double valor,
+            LocalDate data
     ) {
         return new Pagamento(
-                valor
+                valor,
+                data
         );
     }
 }
--- test/contas/PagamentoTest.java
+++ test/contas/PagamentoTest.java
@@ -3,16 +3,29 @@ package contas;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PagamentoTest {
 
     @Test
     @DisplayName("Dado que um valor foi fornecido na criação de um pagamento, esse mesmo valor deveria constar no pagamento")
-    void testCriacaoPagamento() {
+    void testValorInformadoPagamento() {
+        LocalDate data = LocalDate.parse("2024-05-25");
         double valor = 200.0;
-        Pagamento pagamento = Pagamento.build(valor);
+        Pagamento pagamento = Pagamento.build(valor, data);
 
         assertEquals(pagamento.getValor(), valor);
+    }
+
+    @Test
+    @DisplayName("Dado que uma data foi fornecido na criação de um pagamento, essa mesma data deveria constar no pagamento")
+    void testDataInformadaPagamento() {
+        LocalDate data = LocalDate.parse("2024-05-25");
+        double valor = 200.0;
+        Pagamento pagamento = Pagamento.build(valor, data);
+
+        assertEquals(pagamento.getData(), data);
     }
 }
