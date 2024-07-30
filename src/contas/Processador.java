d5b29d0cb1eab2c37abd873e751be761347e8a7a
rodrigopg <rodrigopgrodrigues@outlook.com>
2024-07-30 10:16:36 -0300

    test: Dado que uma fatura foi fornecida para o processador, quando ele fosse criado, a fatura deveria ser igual a fornecida

--- src/contas/Processador.java
+++ src/contas/Processador.java
@@ -4,11 +4,18 @@ import java.util.List;
 
 public class Processador {
     private List<Conta> contas;
+    private Fatura fatura;
 
     public Processador(
-            List<Conta> contas
+            List<Conta> contas,
+            Fatura fatura
     ) {
         this.contas = contas;
+        this.fatura = fatura;
+    }
+
+    public Fatura getFatura() {
+        return fatura;
     }
 
     public List<Conta> getContas() {
--- test/contas/ProcessadorTest.java
+++ test/contas/ProcessadorTest.java
@@ -3,6 +3,7 @@ package contas;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,28 +15,63 @@ class ProcessadorTest {
     @Test
     @DisplayName("Dado atributos válidos e requeridos, quando o processador fosse instanciado, a criação deveria ocorrer sem problemas")
     void testCriacaoProcessador() {
+        LocalDate data = LocalDate.parse("2024-05-25");
+        Fatura fatura = Fatura.build(
+                "Rodrigo",
+                205.90,
+                data
+        );
         List<Conta> contas = Arrays.asList(
                 Conta.build(1L, 300.0),
                 Conta.build(2L, 500.0),
                 Conta.build(3L, 650.0)
         );
-        Processador processador = new Processador(contas);
+        Processador processador = new Processador(contas, fatura);
 
         assertInstanceOf(Processador.class, processador);
     }
 
     @Test
-    @DisplayName("Dado que o processador foi criado com uma lista de contas, quando o processador fosse criado, as contas deveriam iguais as fornecidas")
+    @DisplayName("Dado que uma lista de contas foi fornecida para a processador, quando ele fosse criado, as contas deveriam iguais as fornecidas")
     void testContasProcessador() {
+        LocalDate data = LocalDate.parse("2024-05-25");
+        Fatura fatura = Fatura.build(
+                "Rodrigo",
+                205.90,
+                data
+        );
         List<Conta> contas = Arrays.asList(
                 Conta.build(1L, 300.0),
                 Conta.build(2L, 500.0),
                 Conta.build(3L, 650.0)
         );
         Processador processador = new Processador(
-                contas
+                contas,
+                fatura
         );
 
         assertEquals(processador.getContas(), contas);
+    }
+
+    @Test
+    @DisplayName("Dado que uma fatura foi fornecida para o processador, quando ele fosse criado, a fatura deveria ser igual a fornecida")
+    void testFaturaProcessador() {
+        LocalDate data = LocalDate.parse("2024-05-25");
+        Fatura fatura = Fatura.build(
+                "Rodrigo",
+                205.90,
+                data
+        );
+        List<Conta> contas = Arrays.asList(
+                Conta.build(1L, 300.0),
+                Conta.build(2L, 500.0),
+                Conta.build(3L, 650.0)
+        );
+        Processador processador = new Processador(
+                contas,
+                fatura
+        );
+
+        assertEquals(processador.getFatura(), fatura);
     }
 }
