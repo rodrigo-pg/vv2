e58a369c66f389caf1cbe17fe20f04e601b066ce
rodrigopg <rodrigopgrodrigues@outlook.com>
2024-07-30 10:07:42 -0300

    test: Dado que o processador foi criado com uma lista de contas, quando as contas fossem consultadas, deveriam ser as mesmas da criação

--- src/contas/Processador.java
+++ src/contas/Processador.java
@@ -1,4 +1,22 @@
 package contas;
 
+import java.util.List;
+
 public class Processador {
+    private List<Conta> contas;
+
+    private Processador(
+            List<Conta> contas
+    ) {
+        this.contas = contas;
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+    public static Processador build(List<Conta> contas) {
+        return new Processador(contas);
+    }
+
 }
--- test/contas/ProcessadorTest.java
+++ test/contas/ProcessadorTest.java
@@ -3,6 +3,10 @@ package contas;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProcessadorTest {
@@ -10,8 +14,28 @@ class ProcessadorTest {
     @Test
     @DisplayName("Dado atributos válidos e requeridos, quando o processador fosse instanciado, a criação deveria ocorrer sem problemas")
     void testCriacaoProcessador() {
-        Processador processador = new Processador();
+        List<Conta> contas = Arrays.asList(
+                Conta.build(1L, 300.0),
+                Conta.build(2L, 500.0),
+                Conta.build(3L, 650.0)
+        );
+        Processador processador = Processador.build(contas);
 
         assertInstanceOf(Processador.class, processador);
+    }
+
+    @Test
+    @DisplayName("Dado que o processador foi criado com uma lista de contas, quando as contas fossem consultadas, deveriam ser as mesmas da criação")
+    void testContasProcessador() {
+        List<Conta> contas = Arrays.asList(
+                Conta.build(1L, 300.0),
+                Conta.build(2L, 500.0),
+                Conta.build(3L, 650.0)
+        );
+        Processador processador = Processador.build(
+                contas
+        );
+
+        assertEquals(processador.getContas(), contas);
     }
 }
