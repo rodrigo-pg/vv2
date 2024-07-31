f60c0dea2f45e6782347aed3a771b466b2b92bd2
ThayaneBarros <thayane.barros.ext@synchro.com.br>
2024-07-30 22:48:52 -0300

    test: criação dos testes de relatorio e gerarRelatorio

---
+++ SistemaDeIngressos/src/test/RelatorioTest.java
@@ -0,0 +1,54 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.Relatorio;
+import main.StatusFinanceiro;
+import main.TipoIngresso;
+
+public class RelatorioTest {
+	
+private Relatorio relatorio;
+	
+	@BeforeEach
+    void setUp() {
+		Map<TipoIngresso, Integer> numIngressosVendidos = new HashMap<>();
+        numIngressosVendidos.put(TipoIngresso.VIP, 100);
+        numIngressosVendidos.put(TipoIngresso.MEIA_ENTRADA, 50);
+        numIngressosVendidos.put(TipoIngresso.NORMAL, 350);
+
+		relatorio = new Relatorio(numIngressosVendidos, 1625.00, StatusFinanceiro.LUCRO);
+    }
+	
+	
+	@Test
+	void testCriacaoRelatorio() {
+		assertEquals(100, relatorio.getNumeroIngressosVIP());
+	    assertEquals(50, relatorio.getNumeroIngressosMeiaEntrada());
+	    assertEquals(350, relatorio.getNumeroIngressosNormal());
+	    assertEquals(1625.00, relatorio.getReceitaLiquida(), 0.01);
+	    assertEquals(StatusFinanceiro.LUCRO, relatorio.getStatusFinanceiro());
+	}
+	
+	@Test
+    public void testStatusFinanceiroPrejuizo() {
+        Map<TipoIngresso, Integer> numIngressosVendidos = new HashMap<>();
+        numIngressosVendidos.put(TipoIngresso.VIP, 60);
+        numIngressosVendidos.put(TipoIngresso.MEIA_ENTRADA, 30);
+        numIngressosVendidos.put(TipoIngresso.NORMAL, 240);
+
+        Double receitaLiquida = -500.00;
+        StatusFinanceiro status = StatusFinanceiro.PREJUÍZO;
+
+        Relatorio relatorio = new Relatorio(numIngressosVendidos, receitaLiquida, status);
+
+        assertEquals(StatusFinanceiro.PREJUÍZO, relatorio.getStatusFinanceiro());
+    }
+
+}
--- SistemaDeIngressos/src/test/ShowTest.java
+++ SistemaDeIngressos/src/test/ShowTest.java
@@ -5,20 +5,31 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import main.Ingresso;
+import main.LoteIngressos;
+import main.Relatorio;
 import main.Show;
+import main.StatusFinanceiro;
+import main.TipoIngresso;
 
 
 public class ShowTest {
 	private Show show;
+	private LoteIngressos lote;
+	private List<Ingresso> ingressos;
 	
 	@BeforeEach
     void setUp() {
-		show = new Show("29/07/2024", "Artista Teste", 1000.0, 2000.0, new ArrayList<>(), true);
+		ingressos = preparaLoteIngresso();
+        lote = new LoteIngressos(1, ingressos, 15.0);
+		show = new Show("29/07/2024", "Artista Teste", 1000.0, 2000.0, lote, true);
     }
+	
 	
 	@Test
 	void testCriacaoShow() {
@@ -31,5 +42,43 @@ public class ShowTest {
 	}
 	
 	
+	@Test
+	public void testGerarRelatorioBasico() {
+		List<Ingresso> ingressos = preparaLoteIngresso();
+		LoteIngressos lote = new LoteIngressos(1, ingressos, 15.0);
+		lote.aplicarDesconto();
+        
+	    show.adicionarLote(lote);
 
+	    Relatorio relatorio = show.gerarRelatorio();
+
+	    assertEquals(100, relatorio.getNumeroIngressosVIP());
+	    assertEquals(50, relatorio.getNumeroIngressosMeiaEntrada());
+	    assertEquals(350, relatorio.getNumeroIngressosNormal());
+	    assertEquals(1625.0, relatorio.getReceitaLiquida(), 0.01);
+	    assertEquals(StatusFinanceiro.LUCRO, relatorio.getStatusFinanceiro());
+	}
+	
+	private List<Ingresso> preparaLoteIngresso() {
+		int totalIngressos = 500;
+
+        List<Ingresso> ingressos = new ArrayList<>();
+        for (int i = 0; i < totalIngressos; i++) {
+            if (i < 100) {
+                ingressos.add(new Ingresso(i, TipoIngresso.VIP, 10.0));
+            } else if (i < 150) {
+                ingressos.add(new Ingresso(i, TipoIngresso.MEIA_ENTRADA, 10.0));
+            } else {
+                ingressos.add(new Ingresso(i, TipoIngresso.NORMAL, 10.0));
+            }
+        }
+        
+        for(Ingresso ingresso: ingressos) {
+        	ingresso.marcarComoVendido();
+        }
+        
+        return ingressos;
+		
+	}
+	
 }
