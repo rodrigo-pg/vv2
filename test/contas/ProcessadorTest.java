5fa0ae64ba4de18923bdafed4005d330ea33d7f1
rodrigopg <rodrigopgrodrigues@outlook.com>
2024-07-31 09:54:18 -0300

    test: cenário 3 do processador de contas

--- test/contas/ProcessadorTest.java
+++ test/contas/ProcessadorTest.java
@@ -122,4 +122,26 @@ class ProcessadorTest {
 
         assertEquals("PAGA", processador.getFatura().getStatus());
     }
+
+    @Test
+    void testCaso3Processador() {
+        Fatura fatura = Fatura.build(
+                "Rodrigo",
+                1500.0,
+                LocalDate.parse("2023-02-20"),
+                "PENDENTE"
+        );
+        List<Conta> contas = Arrays.asList(
+                Conta.build(1L, 700.0, LocalDate.parse("2023-02-06"), TipoPagamento.CARTAO_CREDITO),
+                Conta.build(2L, 800.0, LocalDate.parse("2023-02-17"), TipoPagamento.TRANSFERENCIA)
+        );
+        Processador processador = new Processador(
+                contas,
+                fatura
+        );
+
+        processador.processar();
+
+        assertEquals("PENDENTE", processador.getFatura().getStatus());
+    }
 }
